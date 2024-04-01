c84d823c25e5f30c62288334de044c507b2d556f
edu <126445500+iHadesj@users.noreply.github.com>
2024-04-01 15:41:30 -0300

    Add files via upload

---
+++ OperadoresAritimeticos/src/OperadoresAritimeticos.java
@@ -0,0 +1,11 @@
+public class OperadoresAritimeticos {
+    public static void main(String[] args) {
+        int nota1 = 4;
+        int nota2 = 7;
+        int nota3 = 9;
+        int nota4 = 3;
+        int media = nota1 + nota2 + nota3 + nota4/4;
+        
+        System.out.println(media);
+     }
+}
